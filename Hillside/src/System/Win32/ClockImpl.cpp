f7ec270ae34a9aea5dca1dd0bca3e509cc7e2bae
Edwin Wallin <edwin.wallin@dequilla.com>
2018-10-30 08:51:16 +0100

    Performence clock on windows, might not work

--- Hillside/src/System/Win32/ClockImpl.cpp
+++ Hillside/src/System/Win32/ClockImpl.cpp
@@ -5,4 +5,12 @@ hs::Time hs::priv::ClockImpl::getCurrentTime()
 {
 	LARGE_INTEGER time;
 	QueryPerformanceCounter(&time);
+
+	LARGE_INTEGER frequency;
+	QueryPerformanceFrequency(&frequency);
+
+	double freq = static_cast<double>(frequency.QuadPart) / 1000000.0;
+	int64 microseconds = static_cast<int64>((static_cast<double>(time.QuadPart) / freq));
+
+	return hs::Time(microseconds);
 }
